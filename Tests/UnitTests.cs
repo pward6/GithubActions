60aa45220d61456da3038ee64d307aeca07e319e
Preston Ward <pward6@huskers.unl.edu>
2024-11-04 14:42:06 -0600

    added test cases for rest of methods

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,8 +30,85 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-       // Implement 3 tests per operation, following a similar pattern as above
-       [Test]
+        // Implement 3 tests per operation, following a similar pattern as above
+
+        [Test]
+        public void Subtract_Valid_Ward()
+        {
+            Assert.AreEqual(7, Program.Subtract("10", "3"));
+            Assert.AreEqual(7, Program.Subtract("12", "5"));
+            Assert.AreEqual(79, Program.Subtract("81", "2"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Ward()
+        {
+            // this test is initially designed to fail
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Ward()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+
+        [Test]
+        public void Multiply_Valid_Ward()
+        {
+            Assert.AreEqual(30, Program.Multiply("10", "3"));
+            Assert.AreEqual(60, Program.Multiply("12", "5"));
+            Assert.AreEqual(162, Program.Multiply("81", "2"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Ward()
+        {
+            // this test is initially designed to fail
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Ward()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Ward()
+        {
+            Assert.AreEqual(10, Program.Divide("30", "3"));
+            Assert.AreEqual(2, Program.Divide("12", "6"));
+            Assert.AreEqual(9, Program.Divide("81", "9"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Ward()
+        {
+            // this test is initially designed to fail
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Ward()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
         public void Power_Valid_Ward()
         {
             Assert.AreEqual(27, Program.Power("3", "3"));
